385c01cbf3da14cbb7edc6246a837ac0110bd482
Jacob <jacobgutierrez752@gmail.com>
2024-03-06 14:08:31 -0800

    Update database.py
    Provides proper return values, according to project requirements

--- src/database.py
+++ src/database.py
@@ -1,11 +1,12 @@
 # database.py
 
 # Manages creation and loading of database
-# TODO - ask where the test folders will be located
 
 from pathlib import Path
+
+import mysql.connector.errors
 from mysql.connector.abstracts import MySQLCursorAbstract
-from .parsing import format_list
+from .parsing import format_list, format_table
 
 
 def _drop_all_tables(cursor):
@@ -30,67 +31,88 @@ def _load_schema(cursor):
             cursor.execute(query)
 
 
-def _load_user(cursor: MySQLCursorAbstract, folder_name):
+def _load_user(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'users.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO User (UCINetID, firstName, middleName, lastName) 
+            INSERT INTO Users (UCINetID, FirstName, MiddleName, LastName) 
             VALUES (%s, %s, %s, %s)""", values)
+        count += 1
+    return count
 
 
-def _load_admin(cursor: MySQLCursorAbstract, folder_name):
+def _load_admin(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'admins.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO Administrator (UCINetID) 
+            INSERT INTO Administrators (UCINetID) 
             VALUES (%s)""", values)
+        count += 1
+    return count
 
 
-def _load_student(cursor: MySQLCursorAbstract, folder_name):
+def _load_student(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'students.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO Student (UCINetID) 
+            INSERT INTO Students (UCINetID) 
             VALUES (%s)""", values)
+        count += 1
+    return count
 
 
-def _load_emails(cursor: MySQLCursorAbstract, folder_name):
+def _load_emails(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'emails.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO UserEmails (UCINetID, email) 
+            INSERT INTO UserEmails (UCINetID, Email) 
             VALUES (%s, %s)""", values)
+        count += 1
+    return count
 
 
-def _load_courses(cursor: MySQLCursorAbstract, folder_name):
+def _load_courses(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'courses.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO Course (courseID, title, quarter) 
+            INSERT INTO Courses (CourseID, Title, Quarter) 
             VALUES (%s, %s, %s)""", values)
+        count += 1
+    return count
 
 
-def _load_project(cursor: MySQLCursorAbstract, folder_name):
+def _load_project(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'projects.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO Project (projectID, name, description, courseID) 
+            INSERT INTO Projects (ProjectID, Name, Description, CourseID) 
             VALUES (%s, %s, %s, %s)""", values)
+        count += 1
+    return count
 
 
-def _load_machine(cursor: MySQLCursorAbstract, folder_name):
+def _load_machine(cursor: MySQLCursorAbstract, folder_name) -> int:
     path = Path(__file__).parent.parent / folder_name / 'machines.csv'
+    count = 0
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO Machine (machineID, hostname, IPAddress, operationalStatus, location) 
+            INSERT INTO Machines (MachineID, Hostname, IPAddress, OperationalStatus, Location) 
             VALUES (%s, %s, %s, %s, %s)""", values)
+        count += 1
+    return count
 
 
 def _load_use(cursor: MySQLCursorAbstract, folder_name):
     path = Path(__file__).parent.parent / folder_name / 'use.csv'
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO studentUse (projectID, UCINetID, machineID, startDate, endDate)
+            INSERT INTO StudentUseMachinesInProject (ProjectID, StudentUCINetID, MachineID, StartDate, EndDate)
             VALUES (%s, %s, %s, %s, %s)""", values)
 
 
@@ -98,30 +120,43 @@ def _load_manage(cursor: MySQLCursorAbstract, folder_name):
     path = Path(__file__).parent.parent / folder_name / 'manage.csv'
     for values in _load_values_from_file(path):
         cursor.execute("""
-            INSERT INTO manages (UCINetID, machineID) 
+            INSERT INTO AdministratorManageMachines (AdministratorUCINetID, MachineID) 
             VALUES (%s, %s)""", values)
 
 
-def load_database(cursor: MySQLCursorAbstract, folder_name: str):
+def load_database(cursor: MySQLCursorAbstract, folder_name: str) -> str:
     """
     Loads the data of the folder into the database for use
     :param cursor: cursor object
-    :param folder_name: Folder to read from. Assumes all files in folder are existent and readable
+    :param folder_name: Folder to read from.
     :return:
     """
+    # Default value for counts of nothing was added
+    counts = [0, 0, 0]
+
+    # do nothing if not a dir
+    if not (Path(__file__).parent.parent / folder_name).is_dir():
+        raise NotADirectoryError
+
     # Drops the tables currently existing in the database
     _drop_all_tables(cursor)
     _load_schema(cursor)
 
-    _load_user(cursor, folder_name)
-    _load_student(cursor, folder_name)
-    _load_emails(cursor, folder_name)
-    _load_admin(cursor, folder_name)
-    _load_courses(cursor, folder_name)
-    _load_project(cursor, folder_name)
-    _load_machine(cursor, folder_name)
+    # some values are not used, but are counted anyway
+    user_count = _load_user(cursor, folder_name)
+    student_count = _load_student(cursor, folder_name)
+    email_count = _load_emails(cursor, folder_name)
+    admin_count = _load_admin(cursor, folder_name)
+    course_count = _load_courses(cursor, folder_name)
+    project_count = _load_project(cursor, folder_name)
+    machine_count = _load_machine(cursor, folder_name)
     _load_use(cursor, folder_name)
     _load_manage(cursor, folder_name)
+
+    # Some error when reading
+
+    counts = [user_count, machine_count, course_count]
+    return format_table(counts)
 
 
 def create_database(cursor: MySQLCursorAbstract, database_name):
@@ -134,5 +169,4 @@ def _load_values_from_file(path: Path) -> list[str]:
     with open(path, 'r') as f:
         for line in f.readlines():
             values = line.strip().split(',')
-            # Replace 'NULL' strings with None for NULL values in a case-insensitive manner
             yield format_list(values)
--- src/parsing.py
+++ src/parsing.py
@@ -10,3 +10,12 @@ def format_list(values: list[Any]) -> list[Any]:
     :return: list with 'Null' replaced with None
     """
     return [None if value.upper() == 'NULL' else value for value in values]
+
+
+def format_table(values: list[Any]) -> str:
+    """
+    Returns a comma separated string of values
+    :param values: Sequence of non None values that can be cast into a string
+    :return: formatted string of values
+    """
+    return ','.join([str(e) for e in values])
